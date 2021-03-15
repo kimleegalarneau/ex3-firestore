201b73b25cd8a4ea5e5edd75b1d0a1f407214981
Kimlee Galarneau <kimlee.ny@hotmail.com>
2021-03-15 19:08:50 -0400

    configuration firebase

--- src/data/firebase.js
+++ src/data/firebase.js
@@ -3,6 +3,15 @@ import 'firebase/firestore';
 
 /******* Ex#3 - Étape B ********************************/ 
 // Ajouter votre objet de configuration Firebase
+const firebaseConfig = {
+  apiKey: "AIzaSyB4hT4OAlt2GkN2t9yi8UXk6YZUgJ8tbuQ",
+  authDomain: "veille-technologique-b83cd.firebaseapp.com",
+  databaseURL: "https://veille-technologique-b83cd-default-rtdb.firebaseio.com",
+  projectId: "veille-technologique-b83cd",
+  storageBucket: "veille-technologique-b83cd.appspot.com",
+  messagingSenderId: "262670044254",
+  appId: "1:262670044254:web:ec4f6219670f500f5038cf"
+};
 
 // Initialiser Firebase
 if(!fbAppli.apps.length) {
